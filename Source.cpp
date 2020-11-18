7cd224b4866864c669ecb18669dd2f20bdae3873
Parselian <parselian.study@yandex.ru>
2020-11-18 18:56:41 +0300

    re-optimized script

--- Source.cpp
+++ Source.cpp
@@ -2,29 +2,46 @@
 
 using namespace std;
 
+void findPrime(int);
+
 int main() {
-	int num, dividend, divider, flag = 0;
+	int num;
 
 	cout << "enter a number \n";
 	cin >> num;
 
+	if (num < 4) {
+		for (int counter = 2; counter <= num; counter++) {
+			cout << "prime num: " << counter << "\n";
+		}
+		return 0;
+	}
+	else {
+		cout << "prime num: 2" << "\n";
+		findPrime(num);
+	}
 
-	for (dividend = 2; dividend < num; dividend++) 
+	return 0;
+}
+
+void findPrime(int num)
+{
+	int dividend, divider, flag = 0;
+
+	for (dividend = 3; dividend < num; dividend += 2)
 	{
 		flag = 1;
-		for (divider = 2; divider <= sqrt(dividend); divider++)  
+		for (divider = 3; divider <= sqrt(dividend); divider++)
 		{
 			if (dividend % divider == 0)
 			{
 				flag = 0;
 				break;
 			}
-		}	
+		}
 
 		if (flag) {
 			cout << "prime num: " << dividend << "\n";
 		}
 	}
-
-	return 0;
 }
